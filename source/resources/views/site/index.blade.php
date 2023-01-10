200af390c28cc271ba4a5425d12f71b3ea99c9c9
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-01-10 17:22:09 -0300

    criando a view de interação

---
+++ source/resources/views/site/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <section class="container">
+            <div class="d-flex justify-content-center" style="height:100vh;">
+                <div class="m-auto">
+                    <a href="#" class="btn btn-success">Carreguei com sucesso!</a>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+
+    <footer>
+
+    </footer>
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
